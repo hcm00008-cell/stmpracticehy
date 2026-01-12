22516946d3967905dd88bdaa4521107cb528b89a
hcm00008-cell <hcm00008@gmail.com>
2026-01-12 11:45:04 +0000

    scret 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ import pandas as pd
 st.title("1️⃣ ✅ 공개 Google Sheet 읽기")
 st.info("📘 누구나 볼 수 있도록 공개된 시트를 Pandas로 직접 불러오는 가장 간단한 방법입니다.\n📎 링크는 반드시 `export?format=csv` 형태로 설정하세요.")
 
-csv_url1 = "https://docs.google.com/spreadsheets/d/1VC_q8HJfIufjGVR2zGRcJjBgkefIbp6Pv01rQ1uvoXI/export?format=csv"
+csv_url1 = st.secrets["connections"]["gsheets"]["public_url"]
 df1 = pd.read_csv(csv_url1)
 st.subheader("Choice 값 막대그래프")
 choice_counts = df1["choice"].value_counts()
@@ -24,3 +24,4 @@ st.bar_chart(choice_counts)
 
 st.subheader("Choice 값 (카운트)")
 st.dataframe(choice_counts.rename_axis("choice").reset_index(name="count"))
+st.write(st.secrets["openai"]["api_key"])
